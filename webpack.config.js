8b3ad99e2cce6c5410c6461085a571bb64b97291
Brandon Stull <bstull@ucf.edu>
2025-12-03 15:44:42 -0500

    Added Python score module, updated MWDK dependency and script usage to v3.0.4, added update management metadata.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const outputPath = path.join(process.cwd(), 'build')
 
 // load the reusable legacy webpack config from materia-widget-dev
 const wp = require('materia-widget-development-kit/webpack-widget')
-const entries = wp.getDefaultEntries()
 const copy = wp.getDefaultCopyList()
 
 const copyConfig = [
@@ -16,14 +15,24 @@ const copyConfig = [
 	}
 ]
 
-entries['puzzle.js'] = [path.join(srcPath, 'puzzle.coffee')]
-entries['guides/creator.temp.html'] = [path.join(srcPath, '_guides','creator.md')]
-entries['guides/player.temp.html'] = [path.join(srcPath, '_guides','player.md')]
+const entries = {
+	'creator': [
+		path.join(srcPath, 'creator.html'),
+		path.join(srcPath, 'creator.scss'),
+		path.join(srcPath, 'puzzle.coffee'),
+		path.join(srcPath, 'creator.coffee')
+	],
+	'player': [
+		path.join(srcPath, 'player.html'),
+		path.join(srcPath, 'player.scss'),
+		path.join(srcPath, 'puzzle.coffee'),
+		path.join(srcPath, 'player.coffee')
+	]
+}
 
 const options = {
 	copyList: copyConfig,
 	entries: entries
-
 }
 
 module.exports = wp.getLegacyWidgetBuildConfig(options)
